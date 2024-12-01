d507741c741f9daefb9ad4498654ea0b7240c8c7
Christiannguyen1 <110247128+Christiannguyen1@users.noreply.github.com>
2024-11-30 18:13:10 -0600

    Added song items

--- Music-Project/src/components/DisplayHome.jsx
+++ Music-Project/src/components/DisplayHome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { albumsData } from "../assets/assets";
+import { albumsData, songsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
     return (
@@ -11,6 +12,12 @@ const DisplayHome = () => {
                 <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
                 <div className='flex overflow-auto'>
                     {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id} />))}
+                </div>
+            </div>
+            <div className='mb-4'>
+                <h1 className='my-5 font-bold text-2xl'>Toda's Biggest Hits</h1>
+                <div className='flex overflow-auto'>
+                    {songsData.map((item, index) => (<SongItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id} />))}
                 </div>
             </div>
         </>
