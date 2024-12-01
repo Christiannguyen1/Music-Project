74245d002963c7506f5ec4b25be8717230db71f8
Christiannguyen1 <110247128+Christiannguyen1@users.noreply.github.com>
2024-12-01 14:12:45 -0600

    Music Player functionality

---
+++ Music-Project/src/context/PlayerContext.jsx
@@ -0,0 +1,76 @@
+import { createContext } from "react"
+import { useRef } from "react"
+import { useState } from "react"
+import { songsData } from "../assets/assets";
+import { useEffect } from "react";
+
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+
+    const audioRef = useRef();
+    const seekBar = useRef();
+    const seekBg = useRef();
+
+    const [track, setTrack] = useState(songsData[1]);
+    const [playerStatus, setPlayerStatus] = useState(false);
+    const [time, setTime] = useState({
+        currentTime: {
+            second: 0,
+            minute: 0
+        },
+        totalTime: {
+            second: 0,
+            minute: 0
+        }
+    })
+
+    const play = () => {
+        audioRef.current.play();
+        setPlayerStatus(true);
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayerStatus(false);
+    }
+    
+    useEffect(() => {
+        setTimeout(() => {
+            audioRef.current.ontimeupdate = () => {
+                setTime({
+                    currentTime: {
+                        second: Math.floor(audioRef.current.currentTime % 60),
+                        minute: Math.floor(audioRef.current.currentTime / 60)
+                    },
+                    totalTime: {
+                        second: Math.floor(audioRef.current.duration % 60),
+                        minute: Math.floor(audioRef.current.duration / 60)
+                    }
+                })
+            }
+        }, 1000)
+    },[audioRef])
+
+    const contextValue = {
+        audioRef,
+        seekBar,
+        seekBg,
+        track,
+        setTrack,
+        playerStatus, setPlayerStatus,
+        time, setTime,
+        play,
+        pause
+    }
+
+    return (
+        <PlayerContext.Provider value={contextValue}>
+            {props.children}
+        </PlayerContext.Provider>
+    )
+
+}
+
+export default PlayerContextProvider;
