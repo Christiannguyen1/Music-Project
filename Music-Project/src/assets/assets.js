b92876370d0597919f91545944480e647b2c6b93
Christiannguyen1 <110247128+Christiannguyen1@users.noreply.github.com>
2024-12-02 15:59:28 -0600

    Updated Images

--- Music-Project/src/assets/assets.js
+++ Music-Project/src/assets/assets.js
@@ -79,14 +79,14 @@ export const albumsData = [
     },
     {   
         id:1,
-        name: "Top 50 India",
+        name: "Top 50 USA",
         image: img9,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#22543d"
     },
     {   
         id:2,
-        name: "Trending India",
+        name: "Trending USA",
         image: img10,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#742a2a"
@@ -100,14 +100,14 @@ export const albumsData = [
     },
     {   
         id:4,
-        name: "Mega Hits,",
+        name: "Mega Hits",
         image: img11,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#234e52"
     },
     {   
         id:5,
-        name: "Happy Favorites",
+        name: "Today's Top Hits",
         image: img15,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#744210"
