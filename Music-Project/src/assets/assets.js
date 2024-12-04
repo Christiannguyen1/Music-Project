c292f6500d0293b23f3bdc9ced9a5f6e21aaecec
Christiannguyen1 <110247128+Christiannguyen1@users.noreply.github.com>
2024-12-04 15:55:57 -0600

    Filter Icon

--- Music-Project/src/assets/assets.js
+++ Music-Project/src/assets/assets.js
@@ -22,6 +22,7 @@ import arrow_right from './right_arrow.png'
 import arrow_left from './left_arrow.png'
 import spotify_logo from './spotify_logo.png'
 import clock_icon from './clock_icon.png'
+import filter_icon from './filter.png'
 import img1 from './img1.jpg'
 import img2 from './img2.jpg'
 import img3 from './img3.jpg'
@@ -71,7 +72,8 @@ export const assets = {
     arrow_left,
     arrow_right,
     spotify_logo,
-    clock_icon
+    clock_icon,
+    filter_icon
 }
 
 export const albumsData = [
